163102b578fc82f1ca212ea1711c7ed90fbcd511
Henry Guamán <37030669+radamantis1983@users.noreply.github.com>
2018-09-05 21:51:49 -0500

    Add files via upload

---
+++ movilidad1/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * DB configuration variables
+ */
+define('db_host', 'localhost');
+define('db_user', 'id4018870_movilidad');
+define('db_password','mcnamara1');
+define('db_database', 'id4018870_datosgps');
+
+
+?>
