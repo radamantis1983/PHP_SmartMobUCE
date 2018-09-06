f94f5e6d2456ecda2eb540d9e18341b8c321239c
Henry Guamán <37030669+radamantis1983@users.noreply.github.com>
2018-09-05 22:01:39 -0500

    Update config.php

--- movilidad1/config.php
+++ movilidad1/config.php
@@ -3,9 +3,9 @@
  * DB configuration variables
  */
 define('db_host', 'localhost');
-define('db_user', 'id4018870_movilidad');
-define('db_password','mcnamara1');
-define('db_database', 'id4018870_datosgps');
+define('db_user', 'root');
+define('db_password','password');
+define('db_database', 'datosgps');
 
 
 ?>
